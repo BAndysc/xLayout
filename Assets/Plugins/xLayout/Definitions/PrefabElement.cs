10ed3138d53836f8ed6e5eb1169bf2f6908be6d8
Bartosz Korczyński <b.andysc@gmail.com>
2019-07-09 12:29:31 +0200

    Prefab field setters

--- Assets/Plugins/xLayout/Definitions/PrefabElement.cs
+++ Assets/Plugins/xLayout/Definitions/PrefabElement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace xLayout.Definitions
@@ -6,5 +7,23 @@ namespace xLayout.Definitions
     {
         [XmlAttribute]
         public string Path { get; set; }
+        
+        [XmlElement("Setter", typeof(SetterElement))]
+        public List<SetterElement> Setters { get; set; }
+    }
+
+    public class SetterElement
+    {
+        [XmlAttribute]
+        public string Path { get; set; }
+        
+        [XmlAttribute]
+        public string Field { get; set; }
+        
+        [XmlAttribute]
+        public string Component { get; set; }
+        
+        [XmlAttribute]
+        public string Value { get; set; }
     }
 }
